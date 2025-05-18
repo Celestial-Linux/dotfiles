c19d776ea9d1a389ef173670398027a6f797600a
Celeste Peláez <KernelFreeze@users.noreply.github.com>
2025-05-18 18:19:16 -0500

    🔧 (hypr): add initial Hyprland environment and monitor configs; remove wezterm config
    Add env.conf and monitors.conf for Hyprland to set up environment variables
    and monitor layouts, improving system configuration and display management.
    Remove dot_wezterm.lua as wezterm is no longer used or needed, reducing
    configuration clutter and potential conflicts. These changes help streamline
    the setup for a Hyprland-based environment.

--- dot_wezterm.lua
+++
@@ -1,9 +0,0 @@
-local wezterm = require 'wezterm'
-
-local config = wezterm.config_builder()
-
-config.color_scheme = 'Palenight (Gogh)'
-config.front_end = "WebGpu"
-config.font = wezterm.font('Agave Nerd Font')
-config.window_background_opacity = 0.8
-return config
