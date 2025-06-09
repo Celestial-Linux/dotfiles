8d5889d72d1657f072d6f53234552e957b694e21
Celeste Peláez <KernelFreeze@users.noreply.github.com>
2025-06-09 10:26:58 -0500

    🎨 (look.conf): adjust blur settings for improved appearance Change blur size from 1 to 3 and passes from 3 to 2 for a smoother, less resource-intensive effect. Update commented brightness value for future reference.
    ✨ (wezterm.lua): update color scheme and enable WebGPU with NVIDIA adapter
    Switch to 'Catppuccin Mocha' color scheme and set a custom background
    color for better aesthetics. Enable WebGPU front end with explicit
    NVIDIA RTX 3080 adapter configuration for improved performance and
    compatibility. Add new color and font settings for enhanced visual
    clarity and user experience.

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,12 +2,29 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Catppuccin Macchiato'
-config.front_end = "OpenGL"
+config.color_scheme = 'Catppuccin Mocha'
+config.colors = {
+    background = "#2c2a42"
+}
+
+config.webgpu_preferred_adapter = {
+    backend = "Vulkan",
+    device = 8710,
+    device_type = "DiscreteGpu",
+    driver = "NVIDIA",
+    driver_info = "575.57.08",
+    name = "NVIDIA GeForce RTX 3080",
+    vendor = 4318,
+}
+config.front_end = "WebGpu"
+
 config.enable_wayland = true
 config.font = wezterm.font('Agave Nerd Font')
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 config.window_background_opacity = 0.5
+-- config.text_background_opacity = 0.4
+config.bold_brightens_ansi_colors = "BrightOnly"
+-- config.dpi = 192.0
 
 config.hide_tab_bar_if_only_one_tab = true
 
