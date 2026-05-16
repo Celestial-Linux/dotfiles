e07840313e3cf709bcdfc8e6de755952f4c2f814
Celeste Peláez <contact@celestelove.dev>
2026-05-16 01:49:36 -0500

    fix(nvim): recognize Cargo.toml as taplo root

--- dot_config/nvim/lua/configs/lspconfig.lua
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,11 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local servers = { "html", "cssls", "rust_analyzer", "kotlin_lsp", "jdtls", "taplo" }
+
+vim.lsp.config("taplo", {
+  root_markers = { ".taplo.toml", "taplo.toml", "Cargo.toml", ".git" },
+})
+
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
