62965626472e07aceb9f931c8417326f45ae3b1e
Celeste Peláez <contact@celestelove.dev>
2026-05-21 16:22:37 -0500

    chore(hypr): set workspace-specific layouts

--- dot_config/hypr/hyprland/workspaces.lua
+++ dot_config/hypr/hyprland/workspaces.lua
@@ -1,12 +1,19 @@
 local lg_monitor = "desc:LG Electronics 27GL650F 0x00059204"
 local acer_monitor = "desc:Acer Technologies K202HQL T0CAA0028519"
 
+local lg_workspace_layouts = {
+	[2] = "master",
+	[3] = "master",
+	[9] = "monocle",
+}
+
 for workspace = 1, 10 do
 	hl.workspace_rule({
 		workspace = tostring(workspace),
 		persistent = true,
 		monitor = lg_monitor,
 		default = workspace == 1,
+		layout = lg_workspace_layouts[workspace] or "dwindle",
 	})
 end
 
@@ -16,5 +23,6 @@ for workspace = 11, 20 do
 		persistent = true,
 		monitor = acer_monitor,
 		default = workspace == 11,
+		layout = "monocle",
 	})
 end
