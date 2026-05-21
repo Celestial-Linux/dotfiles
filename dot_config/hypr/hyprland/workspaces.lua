69d4973b7a334f9eee61e02e7c14c6b0e61d7b82
Celeste Peláez <contact@celestelove.dev>
2026-05-21 16:36:53 -0500

    chore(hyprland): set workspace 1 default to scrolling layout

--- dot_config/hypr/hyprland/workspaces.lua
+++ dot_config/hypr/hyprland/workspaces.lua
@@ -2,6 +2,7 @@ local lg_monitor = "desc:LG Electronics 27GL650F 0x00059204"
 local acer_monitor = "desc:Acer Technologies K202HQL T0CAA0028519"
 
 local lg_workspace_layouts = {
+	[1] = "scrolling",
 	[2] = "master",
 	[3] = "master",
 	[9] = "monocle",
