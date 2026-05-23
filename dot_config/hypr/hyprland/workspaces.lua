f08962ecbb28ef996892a9edfb22de5afd924b14
Celeste Peláez <contact@celestelove.dev>
2026-05-23 01:05:37 -0500

    feat(hypr): add default workspace names

--- dot_config/hypr/hyprland/workspaces.lua
+++ dot_config/hypr/hyprland/workspaces.lua
@@ -8,12 +8,22 @@ local lg_workspace_layouts = {
 	[9] = "monocle",
 }
 
+local workspace_default_names = {
+	[1] = "browser",
+	[2] = "terminal",
+	[3] = "code",
+	[4] = "music",
+	[11] = "discord",
+	[12] = "whatsapp",
+}
+
 for workspace = 1, 10 do
 	hl.workspace_rule({
 		workspace = tostring(workspace),
 		persistent = true,
 		monitor = lg_monitor,
 		default = workspace == 1,
+		default_name = workspace_default_names[workspace],
 		layout = lg_workspace_layouts[workspace] or "dwindle",
 	})
 end
@@ -24,6 +34,7 @@ for workspace = 11, 20 do
 		persistent = true,
 		monitor = acer_monitor,
 		default = workspace == 11,
+		default_name = workspace_default_names[workspace],
 		layout = "monocle",
 	})
 end
