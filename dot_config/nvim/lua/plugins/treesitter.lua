755d3fd9f425941f993bb5b6c1527e853cfe6465
Celeste Peláez <contact@celestelove.dev>
2026-06-02 00:58:57 -0500

    feat(nvim): switch treesitter plugin to main branch
    Update treesitter lockfile and setup to use main branch plus explicit parser bootstrap.

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,38 @@
+local parsers = {
+  "vim",
+  "lua",
+  "luadoc",
+  "printf",
+  "vimdoc",
+  "query",
+  "toml",
+  "html",
+  "css",
+  "markdown",
+  "markdown_inline",
+}
+
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
+  build = ":TSUpdate",
   opts = {
-    ensure_installed = {
-      "vim",
-      "lua",
-      "vimdoc",
-      "toml",
-      "html",
-      "css",
-    },
+    install_dir = vim.fn.stdpath "data" .. "/site",
   },
+  config = function(_, opts)
+    pcall(function()
+      dofile(vim.g.base46_cache .. "syntax")
+      dofile(vim.g.base46_cache .. "treesitter")
+    end)
+
+    local treesitter = require "nvim-treesitter"
+    treesitter.setup(opts)
+
+    if treesitter.install then
+      treesitter.install(parsers)
+    else
+      require("nvim-treesitter.install").ensure_installed(parsers)
+    end
+  end,
 }
